0f004ed6ce18f7f6f1d736233c2972d1a916fdef
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2023-05-16 23:47:24 -0700

    Update and rename nnGates2.py to Main.py

--- Main.py
+++ Main.py
@@ -1,27 +1,39 @@
 import math
 import numpy as np
 
-sig = lambda x: 1 / (1 + math.exp(-x))
+#sig = lambda x: 1 / (1 + math.exp(-x))
 
-
+'''
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
-
-
-# Inputs for OR gate
+'''
+# Inputs for OR/AND gate
 features = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
 targets = np.array([0, 1, 1, 1]).reshape(4, -1)
 #targets = np.array([0, 0, 1, 0]).reshape(4, -1)
 
 
 
-
+'''
+Multiply weights to the features and add bias before taking the sigmoid of the result
+vectorized sigmoid so it can be applied to all values in the matrix
+returns the cost and the results cost is used whe taking the 'derivative' in the back function
+always good to have results and we print them when done training 
+'''
 def forward(weights, bias, sig, calc, features=features, targets=targets):
     results = sig(features @ weights + bias)
     cost = (sum(calc(results, targets))) / len(results)
     return cost, results
 
-
+'''
+Performing our lazy version of 'backpropagation'
+which really is just taking the finite difference
+epsilon is the value we are adding on to the value f(x+h) - f(x) / h
+eps functions as our h just a small adjustment to the function
+we use this value to adjust our weight and bias i think theoretically we could also
+do it to adjust our learning rate as well but I didn't do that.
+Probbaly a more efficient way of doing this
+'''
 def backward(weights, c, eps, bias, sig, calc, lr):
     rows, cols = weights.shape
     new_weights = np.zeros_like(weights)
@@ -36,7 +48,9 @@ def backward(weights, c, eps, bias, sig, calc, lr):
 
 
 
-
+'''
+Run our simple neural network (neuron) 
+'''
 def main(features=features, targets=targets, iterations=1000):
     # Set basic variables and vectorize basic lambda functions for vectors
     calc = np.vectorize(lambda x, y: (x - y) ** 2)
